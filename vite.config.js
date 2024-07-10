c2fa2edfedb917b5312998c12cd25b15a6886da0
Thiago Correa <thiagocorrea200375@gmail.com>
2024-07-10 15:01:01 -0300

    changes

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: 'public',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  server: {
+    open: true,
+  },
+});
