5a1903f5df2c6d0e3a3106598556743382911f7b
Thiago Correa <thiagocorrea200375@gmail.com>
2024-07-14 19:20:06 -0300

    planet fix

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: 'public',
   build: {
-    outDir: '../dist',
-    emptyOutDir: true,
+    outDir: '../dist', // Diretório de saída para os arquivos construídos
+    emptyOutDir: true, // Limpa o diretório de saída antes de construir
   },
   server: {
     open: true,
