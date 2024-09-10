739f0d0460d41408a19f47ab3d42328c3ca19db7
Thiago Correa <thiagocorrea200375@gmail.com>
2024-09-10 20:59:27 -0300

    fix: fixed carousel position

--- components/AllProjectsCard/AllProjectsCard.tsx
+++ components/AllProjectsCard/AllProjectsCard.tsx
@@ -7,7 +7,7 @@ import IProjectCardProps from "./types"
 const AllProjectsCard = ({position}: IProjectCardProps) => {
   return (
    <div className="w-[300px] min-h-[400px] overflow-hidden mt-5 md:w-[620px] xl:w-[940px] 2xl:w-full 2xl:px-5 2xl:flex 2xl:justify-center">
-      <ul className={`-translate-x-[${position}] flex gap-[20px] w-max h-full transition duration-[400ms] md:mt-5 2xl:translate-x-0`}>
+      <ul style={{transform: `translateX(-${position})`}} className={`flex gap-[20px] w-max h-full transition duration-[400ms] md:mt-5 2xl:translate-x-0`}>
          {AllProjectsDetails.map((project) => (
             <li key={project.id} className="flex flex-col w-[300px] min-h-[400px] border-[1px] border-highlight rounded-[10px]">
                <div className="w-[300px] h-[150px] rounded-t-[10px]" 
