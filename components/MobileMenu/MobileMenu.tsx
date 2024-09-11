63204a50c95e455fa0cc514ad9e261f55c4afa78
Thiago Correa <thiagocorrea200375@gmail.com>
2024-09-10 22:46:41 -0300

    fix: fixed mobile menu on landscape

--- components/MobileMenu/MobileMenu.tsx
+++ components/MobileMenu/MobileMenu.tsx
@@ -15,7 +15,7 @@ const MobileMenu = ({links, isOtherPage}: IHeaderProps) => {
   return (
     <>
       <FontAwesomeIcon onClick={handleClick} className="w-[20px] h-[20px] lg:hidden" icon={active ? faXmark : faBars}/>
-      <div className={`${active && 'translate-x-0'} flex flex-col items-center pt-40 fixed top-[60px] left-0 w-full h-screen bg-background bg-opacity-[0.97] -translate-x-[100%] transition duration-[400ms] ease-in-out landscape:pt-10 md:top-[80px] lg:hidden`}>
+      <div className={`${active && 'translate-x-0'} flex flex-col items-center pt-40 fixed top-[60px] left-0 w-full h-dvh bg-background bg-opacity-[0.97] -translate-x-[100%] transition duration-[400ms] ease-in-out landscape:pt-5 md:top-[80px] lg:hidden`}>
         <HeaderLinks handleClick={handleClick} links={links} className="flex-col items-center" isMobileMenu={true} isOtherPage={isOtherPage}/>
         <SocialMediaLinks/>
       </div>
--- components/SocialMediaLinks/SocialMediaLinks.tsx
+++ components/SocialMediaLinks/SocialMediaLinks.tsx
@@ -3,7 +3,7 @@ import { socialMediaLinks } from "@/data/socialMediaLinks"
 
 const SocialMediaLinks = ({isFooter}: ISocialMediaProps) => {
   return (
-   <ul className={`flex gap-[10px] xxs:gap-[15px] text-highlight ${isFooter ? 'items-center' : 'mt-5 pl-2'}`}>
+   <ul className={`flex gap-[10px] xxs:gap-[15px] text-highlight ${isFooter ? 'items-center' : 'mt-5 pl-2'} landscape:hidden`}>
       {socialMediaLinks.map(link => (
          <li key={link.name}>
             <a href={link.href} className={`${isFooter ? "text-xl" : 'text-[26px]'} transition duration-[400ms] hover:drop-shadow-[0_0_15px_rgba(145,94,255,0.8)]`} target="_blank" aria-label={link.name}>
