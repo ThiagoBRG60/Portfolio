27000f72648349bf7e87f65f80151b64fb39f01f
Thiago Correa <thiagocorrea200375@gmail.com>
2024-09-10 23:37:28 -0300

    remove: removed social links from mobile menu

--- components/MobileMenu/MobileMenu.tsx
+++ components/MobileMenu/MobileMenu.tsx
@@ -3,7 +3,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons"
 import { useState } from "react"
 import HeaderLinks from "../HeaderLinks/HeaderLinks"
-import SocialMediaLinks from "../SocialMediaLinks/SocialMediaLinks"
 
 const MobileMenu = ({links, isOtherPage}: IHeaderProps) => {
   const [active, setActive] = useState(false)
@@ -17,7 +16,6 @@ const MobileMenu = ({links, isOtherPage}: IHeaderProps) => {
       <FontAwesomeIcon onClick={handleClick} className="w-[20px] h-[20px] lg:hidden" icon={active ? faXmark : faBars}/>
       <div className={`${active && 'translate-x-0'} flex flex-col items-center pt-40 fixed top-[60px] left-0 w-full h-dvh bg-background bg-opacity-[0.97] -translate-x-[100%] transition duration-[400ms] ease-in-out landscape:pt-5 md:top-[80px] lg:hidden`}>
         <HeaderLinks handleClick={handleClick} links={links} className="flex-col items-center" isMobileMenu={true} isOtherPage={isOtherPage}/>
-        <SocialMediaLinks/>
       </div>
     </>
   )
